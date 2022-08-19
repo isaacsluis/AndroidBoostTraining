4eb5fef5055b4ab54ed4406e73b60dce33085f00
Luis Isaacs <isaacsluis6@gmail.com>
2022-08-19 12:39:22 -0500

    Adicion de Tag de OnCreate and onDestroy

--- app/src/main/java/com/example/adroindboosttrading/MainActivity.kt
+++ app/src/main/java/com/example/adroindboosttrading/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.adroindboosttrading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity","onCreate")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("MainActivity","onDestroy")
     }
 }
 
-
-
-//comentario
