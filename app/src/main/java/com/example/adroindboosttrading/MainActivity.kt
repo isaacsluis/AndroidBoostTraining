8a79fa1d3d9cbbbd259d1a4af2fbae3d35c0f36f
Luis Isaacs <isaacsluis6@gmail.com>
2022-08-19 14:57:09 -0500

    Agregar componentes y cambio de layout, LinearLayout , boton , EditText, TOAST y la ejecucion del onclicklistener por lamnda

--- app/src/main/java/com/example/adroindboosttrading/MainActivity.kt
+++ app/src/main/java/com/example/adroindboosttrading/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.adroindboosttrading
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.example.adroindboosttrading.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.message.text = "Elemento creado con binding inflado"
+
+//        LLamado por la ejecucion de un boton sin lamda
+//        binding.button.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(p0: View?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
+
+        // LLamado atravez de una lamda
+        binding.button.setOnClickListener {
+            val send = binding.textInput.text
+            Toast.makeText(this,"$send lo que escribio",Toast.LENGTH_LONG).show()
+
+        }
 
         //forma clasica de llamar un componente
         /* setContentView(R.layout.activity_main)
