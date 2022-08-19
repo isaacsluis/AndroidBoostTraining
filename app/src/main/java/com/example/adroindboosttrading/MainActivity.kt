861abe877db281211f50f89931f09ffd43273b73
Luis Isaacs <isaacsluis6@gmail.com>
2022-08-19 13:47:07 -0500

    Acceder a los componentes a partir de Binding

--- app/src/main/java/com/example/adroindboosttrading/MainActivity.kt
+++ app/src/main/java/com/example/adroindboosttrading/MainActivity.kt
@@ -3,20 +3,31 @@ package com.example.adroindboosttrading
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import com.example.adroindboosttrading.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        Log.d("MainActivity","onCreate")
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.message.text = "Elemento creado con binding inflado"
+
+        //forma clasica de llamar un componente
+        /* setContentView(R.layout.activity_main)
+        val message = findViewById<TextView>(R.id.message)
+        message.text = "Ya no quiero saludar a Android"  */
+
+        Log.d("MainActivity","onCreate") // Tag de seguimiento
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.d("MainActivity","onDestroy")
+        Log.d("MainActivity","onDestroy") // Tag de seguimiento
     }
 }
 
